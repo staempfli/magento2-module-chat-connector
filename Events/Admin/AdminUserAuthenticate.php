c55fa4169ae417c8a0b149fb89420ac606568120
Marcel Hauri <marcel@hauri.me>
2017-07-26 10:42:54 +0200

    obscure admin password to not exploit full password

--- Events/Admin/AdminUserAuthenticate.php
+++ Events/Admin/AdminUserAuthenticate.php
@@ -8,12 +8,32 @@
 
 namespace Staempfli\ChatConnector\Events\Admin;
 
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Staempfli\ChatConnector\Events\Events;
 
 class AdminUserAuthenticate extends Events implements ObserverInterface
 {
+    /**
+     * @var \Magento\Framework\App\Request\Http
+     */
+    private $request;
+
+    /**
+     * AdminUserAuthenticate constructor.
+     * @param RequestInterface $request
+     * @param ManagerInterface $eventManager
+     */
+    public function __construct(
+        RequestInterface $request,
+        ManagerInterface $eventManager
+    ) {
+        parent::__construct($eventManager);
+        $this->request = $request;
+    }
+
     /**
      * @param Observer $observer
      * @return void
@@ -22,10 +42,25 @@ class AdminUserAuthenticate extends Events implements ObserverInterface
     {
         if (!$observer->getResult()) {
             $this->notify(__(
-                "<strong>Admin user login failed!</strong>\n Username: %1 \n Password: %2",
+                "<strong>Admin user login failed!</strong>\n Username: %1 \n Password: %2 \n IP: %3",
                 $observer->getUsername(),
-                $observer->getPassword()
+                $this->getObscuredPassword($observer->getPassword()),
+                $this->request->getServer('REMOTE_ADDR')
             ));
         }
+    }
+
+    /**
+     * @param string $password
+     * @return string
+     */
+    private function getObscuredPassword(string $password)
+    {
+        $passwordLength = strlen($password);
+        return sprintf('%s%s%s',
+            substr($password, 0, 1),
+            str_repeat('*', ($passwordLength - 2)),
+            substr($password, -1)
+        );
     }
 }
